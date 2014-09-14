dd52af0d92046f045bac4c12968904048c155927
Zivia <zivia@unm.edu>
2014-09-13 20:28:27 -0600

    Added shell for AntAI class.
    Still needs implementation, but a high level of the AI is described.

--- antworld/ai/AntAI.java
+++ antworld/ai/AntAI.java
@@ -2,9 +2,24 @@ package antworld.ai;
 
 import java.awt.Point;
 
+import antworld.data.AntAction;
+
 public abstract class AntAI
 {
-  // public abstract Enum<E> getJob();
+  public AntAction chooseAction()
+  {
+    //If ant is in Group, then ignore this.
+    //Check if need to retreat.
+    //Check ActivityEnum for current activity.
+      //If ActivityEnum is not IDLE, carry out the next activity.
+      //If ActivityEnum is IDLE then reference PriorityEnum for next activity.
+    //Check PriorityEnum for current priority.
+      //If PriorityEnum is not IDLE, carry out the priority.
+      //If PriorityEnum is IDLE then determine next priority.
+    
+    return null;
+  }
+  
   public abstract void retreatToBase();
 
   public abstract void retreatToLocation();
